d0aec5951f59f0da2ae409706f8cc26c0f6649a0
indra120 <indrapranata120@gmail.com>
2024-02-05 20:35:34 +0800

    Update

--- src/components/transaction-list.tsx
+++ src/components/transaction-list.tsx
@@ -6,28 +6,25 @@ interface TransactionListProps {
   handleDelete: (id: number) => () => void
 }
 
-const TransactionList: React.FC<TransactionListProps> = (props) => {
-  return (
-    <>
-      <h3>History</h3>
+const TransactionList: React.FC<TransactionListProps> = (props) => (
+  <>
+    <h3>History</h3>
 
-      <ul className="list">
-        {props.transactions.map(({ id, text, amount }) => (
-          <li className={`list ${amount < 0 ? "minus" : "plus"}`} key={id}>
-            {text}
-            <span>
-              {amount < 0 ? "-" : ""}$
-              {Math.abs(amount)}
-            </span>
+    <ul className="list">
+      {props.transactions.map(({ id, text, amount }) => (
+        <li className={`list ${amount < 0 ? "minus" : "plus"}`} key={id}>
+          {text}
+          <span>
+            {amount < 0 ? "-" : ""}${Math.abs(amount)}
+          </span>
 
-            <button onClick={props.handleDelete(id)} className="delete-btn">
-              x
-            </button>
-          </li>
-        ))}
-      </ul>
-    </>
-  )
-}
+          <button onClick={props.handleDelete(id)} className="delete-btn">
+            x
+          </button>
+        </li>
+      ))}
+    </ul>
+  </>
+)
 
 export default TransactionList
